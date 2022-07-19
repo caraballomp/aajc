1cafff4b0b8d400821fd0d4bf2611e28268bf202
Michelle Caraballo <caraballomp@yahoo.com>
2022-07-18 19:36:21 -0700

    updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# aajc
-landing page 
--- script.js
+++
@@ -1,6 +0,0 @@
-
-const showcase = document.querySelector('.showcase');
-
-showcase.addEventListener('click', () => {
-  showcase.classList.toggle('active');
-})
